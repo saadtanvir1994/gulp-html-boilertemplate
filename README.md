a6bbfbdc1f749400431ee186ec2cbeb141eb788e
Saad Tanvir <37467079+saadtanvir1994@users.noreply.github.com>
2023-10-05 13:39:45 +0500

    project setup v2

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# gulp-html-boilertemplate
-A boilerplate for building static web projects (like Figma to HTML conversion) with Gulp.js with modular approach and autoprefixer
+# Ensoulee
---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const jsImport = require('gulp-js-import');
+const sourcemaps = require('gulp-sourcemaps');
+const htmlPartial = require('gulp-html-partial');
+const clean = require('gulp-clean');
+const isProd = process.env.NODE_ENV === 'prod';
+
+const htmlFile = [
+    'src/*.html'
+]
+
+function html() {
+    return gulp.src(htmlFile)
+        .pipe(htmlPartial({
+            basePath: 'src/partials/'
+        }))
+        // .pipe(gulpIf(isProd, htmlmin({
+        //     collapseWhitespace: true
+        // })))
+        .pipe(gulp.dest('build'));
+}
+
+function css() {
+    return gulp.src(['src/sass/*.scss'])
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass({
+            includePaths: ['node_modules']
+        }).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions'))
+        //.pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulpIf(isProd, cssmin()))
+        .pipe(gulp.dest('build/css/'));
+}
+
+function cssvendors() {
+    return gulp.src(['src/sass/vendors/*'])
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulp.dest('build/css/vendors/'));
+}
+
+
+function js() {
+    return gulp.src('src/js/vendors/*.js')
+        .pipe(jsImport({
+            hideConsole: true
+        }))
+        .pipe(concat('vendors.min.js'))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('build/js'));
+}
+
+function js2() {
+    return gulp.src('src/js/app.js')
+        .pipe(jsImport({
+            hideConsole: true
+        }))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('build/js'));
+}
+
+function img() {
+    return gulp.src('src/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('build/img/'));
+}
+function fonts() {
+    return gulp.src('src/fonts/*')
+        .pipe(gulp.dest('build/fonts/'));
+}
+
+function serve() {
+    browserSync.init({
+        open: true,
+        server: './build'
+    });
+}
+
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+
+function watchFiles() {
+    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+    gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload));
+    gulp.watch('src/fonts/*.*', gulp.series(fonts, browserSyncReload));
+    gulp.watch('src/js/vendors/*.js', gulp.series(js, browserSyncReload));
+    gulp.watch('src/js/app.js', gulp.series(js2, browserSyncReload));
+    gulp.watch('src/img/**/*.*', gulp.series(img));
+    return;
+}
+
+function del() {
+    return gulp.src('build/*', {read: false})
+        .pipe(clean());
+}
+
+exports.css = css;
+exports.html = html;
+exports.js = js;
+exports.js2 = js2;
+exports.del = del;
+exports.serve = gulp.parallel(html, css, cssvendors, fonts, js,js2, img, watchFiles, serve);
+exports.default = gulp.series(del, html, css, cssvendors,fonts, js,js2, img);
